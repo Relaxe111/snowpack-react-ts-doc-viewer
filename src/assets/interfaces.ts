13cce08468398377417d0547caf801461c97cacc
igor_ef <igor@avans.no>
2020-12-31 00:45:38 +0100

    removed svgs

--- src/assets/interfaces.ts
+++
@@ -1,7 +0,0 @@
-import type Icon from '@ant-design/icons'
-
-type ExtractProps<TComponentOrTProps> = TComponentOrTProps extends React.ComponentType<infer TProps>
-    ? TProps
-    : TComponentOrTProps
-
-export type IconProps = ExtractProps<typeof Icon>
